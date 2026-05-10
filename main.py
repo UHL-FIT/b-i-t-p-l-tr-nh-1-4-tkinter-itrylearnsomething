82854ae5536097b0aedc25369b4c0d34ecb7f1fb
Fumo <162035057+itrylearnsomething@users.noreply.github.com>
2026-05-10 21:42:00 +0700

    main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print('Hello world!')
+import tkinter as tk
+
+root = tk.Tk()
+root.title(" ung dung dau tien")
+root.geometry("500x500")
+
+nhan_chao = tk.Label(root, text="chao mung sinh vien UHL!")
+nhan_chao.pack(padx=50)
+
+root.mainloop()
